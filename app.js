6d911deb60dd18e2aaf059c3dd0ff4303f1968cc
augusto-proano <tito4gsus@gmail.com>
2018-04-19 12:51:47 -0400

    nunjucks up and running

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const chalk = require('chalk');
-const twitterApp = express(); //create an instance of an express application
+const nunjucks = require("nunjucks");
+const twitterApp = express(); 
+
+// Adding Nunjucks Template Engine
+const locals = {
+    title: "What Universe",
+    subtitle: "would you like to live in",
+    people: [
+        {name: "Gandalf"},
+        {name: "Frodo"},
+        {name: "Hermione"},
+        {name: "Yang"},
+        {name: "Augusto"}
+    ]
+};
+
+// Nunjucks and Express Integration
+twitterApp.engine("html", nunjucks.render);
+twitterApp.set("view engine", "html");
+
+
+nunjucks.configure("views", {noCache: true});
+nunjucks.render("index.html", locals, function (err, output){
+    console.log(output);
+})
+const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
 
 twitterApp.use((req, res, next) => {
-    // console.log('the middleware works!');
-    res.send('we sent something');
-    next();
+    res.render('index', locals);
 });
 
 
 twitterApp.get('/cat',(req, res) => res.send('sent you twitterApp stuff! we changed something!'))
 
-
-
-
-
-// This is a test
 
 
 
